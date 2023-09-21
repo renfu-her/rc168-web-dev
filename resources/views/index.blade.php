6f4a385b8c44ef140c4df58e9dfe1834eb9edbb0
Patrick Her <renfu.her@gmail.com>
2023-09-21 17:26:56 +0800

    update qr code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,78 +3,40 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Image Upload with QR Code</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>QR Code</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="container mt-5">
-        <div>
-            <input type="file" accept="image/*" multiple id="pictureSelect" />
-        </div>
-        <div>
-            <span>QR 內容：</span>
-            <a id="result"></a>
-        </div>
-        <div>
-            <canvas id="qrCanvas"></canvas>
-        </div>
 
-        <script>
-            $(function() {
-                $("#pictureSelect").change(function(event) {
-                    var file = event.target.files[0];
-                    if (window.FileReader) {
-                        var fileReader = new FileReader();
-                        fileReader.readAsDataURL(file);
-                        fileReader.onloadend = function(event) {
-                            var base64Data = event.target.result;
-                            base64ToqR(base64Data)
-                        }
-                    }
-
-                })
-            });
-
-            function base64ToqR(data) {
-                var objCanvas = document.getElementById("qrCanvas");
-                var ctx = objCanvas.getContext("2d"); // 返回值是CanvasRenderingContext2D类的对象实例。
-
-                var image = new Image();
-                image.src = data;
-
-                image.onload = function() {
-                    ctx.drawImage(image, 0, 0, image.width, image.height); // 绘图
-
-                    var imageData = ctx.getImageData(0, 0, image.width, image.height);
-
-                    $('#result').attr('width': image.width).attr('height': image.height);
-
-                    console.log(imageData.data, imageData.width, imageData.height);
-
-                    // QR码解析
-                    const code = jsQR(
-                        imageData.data, // 图像数据
-                        imageData.width, // 宽度
-                        imageData.height, // 高度
-                        {
-                            // 可选的对象
-                            inversionAttempts: "dontInvert",
-                        }
-                    );
-
-                    if (code) {
-                        $("#result")[0].innerHTML = code.data;
-                    } else {
-                        $("#result")[0].innerHTML = "";
-                        alert("识别错误")
-                    }
-                };
-            }
-        </script>
+    <div class="container">
+        <div style="width: 500px" id="reader"></div>
     </div>
-    <script src="{{ asset('css/jsQR.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('js/html5-qrcode.min.js') }}"></script>
+
+    <script>
+        function onScanSuccess(decodedText, decodedResult) {
+            // Handle on success condition with the decoded text or result.
+            console.log(`Scan result: ${decodedText}`, decodedResult);
+        }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", {
+                fps: 10,
+                qrbox: 250
+            });
+        html5QrcodeScanner.render(onScanSuccess);
+    </script>
 </body>
 
 </html>
