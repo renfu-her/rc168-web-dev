6efb2ad073232109a2ebbdd0ea29d32a8f7ad08c
Patrick Her <renfu.her@gmail.com>
2023-09-21 15:55:51 +0800

    update qrcode

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,56 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Image Upload with QR Code</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
+
 <body>
     <div class="container mt-5">
-        <form action="/upload" method="post" enctype="multipart/form-data" class="row g-3" id="upload-form">
-            @csrf
-            <div class="col-auto">
-                <label for="image" class="form-label">選擇圖片</label>
-                <input type="file" class="form-control" name="image" id="image" accept="image/*" capture="camera">
-            </div>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary mb-3">上傳</button>
-            </div>
-        </form>
-    </div>
+        <div>
+            <input type="file" accept="image/*" multiple id="pictureSelect" />
+        </div>
+        <div>
+            <span>QR 內容：</span>
+            <a id="result"></a>
+        </div>
+        <div>
+            <canvas id="qrCanvas"></canvas>
+        </div>
 
-    <script src="{{ asset('css/jsQR.js') }}"></script>
-    <script>
-        $(document).ready(function() {
-            $('#image').on('change', function(e) {
-                const file = e.target.files[0];
-
-                reader.onload = function(event) {
-                    const image = new Image();
-                    image.onload = function() {
-                        const canvas = document.createElement('canvas');
-                        canvas.width = image.width;
-                        canvas.height = image.height;
-                        const ctx = canvas.getContext('2d');
-                        ctx.drawImage(image, 0, 0);
-                        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                        const code = jsQR(imageData.data, imageData.width, imageData.height);
-                        if (code) {
-                            console.log("Found QR code", code.data);
-                            // 你可以在這裡添加更多的邏輯，例如將 QR code 的數據添加到表單中
+        <script>
+            $(function() {
+                $("#pictureSelect").change(function(event) {
+                    var file = event.target.files[0];
+                    if (window.FileReader) {
+                        var fileReader = new FileReader();
+                        fileReader.readAsDataURL(file);
+                        fileReader.onloadend = function(event) {
+                            var base64Data = event.target.result;
+                            base64ToqR(base64Data)
                         }
-                    };
-                    image.src = event.target.result;
+                    }
+
+                })
+            });
+
+            function base64ToqR(data) {
+                var objCanvas = document.getElementById("qrCanvas");
+                var ctx = objCanvas.getContext("2d"); // 返回值是CanvasRenderingContext2D类的对象实例。
+
+                var image = new Image();
+                image.src = data;
+
+                image.onload = function() {
+                    ctx.drawImage(image, 0, 0, image.width, image.height); // 绘图
+
+                    var imageData = ctx.getImageData(0, 0, image.width, image.height);
+
+                    // QR码解析
+                    const code = jsQR(
+                        imageData.data, // 图像数据
+                        imageData.width, // 宽度
+                        imageData.height, // 高度
+                        {
+                            // 可选的对象
+                            inversionAttempts: "dontInvert",
+                        }
+                    );
+
+                    if (code) {
+                        $("#result")[0].innerHTML = code.data;
+                    } else {
+                        $("#result")[0].innerHTML = "";
+                        alert("识别错误")
+                    }
                 };
-
-                reader.readAsDataURL(file);
-            });
-
-            $('#upload-form').on('submit', function(e) {
-                // 在這裡，你可以添加更多的上傳邏輯
-            });
-        });
-    </script>
+            }
+        </script>
+    </div>
+    <script src="{{ asset('css/jsQR.js') }}"></script>
 </body>
+
 </html>
