5ff29ca51d5c7247f6dff73d2a09233cd37f66ea
Patrick Her <renfu.her@gmail.com>
2023-09-27 14:19:00 +0800

    update qrcode

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,33 +13,12 @@
 <body>
 
     <div class="container">
-        <div style="width: 300px" id="reader"></div>
-        <div id="qrcode-result"></div>
+        <a href="/qrcode" class="btn btn-outline-primary">掃描 QRCode</a>
+        <a href="/picture" class="btn btn-outline-primary">拍照合拼</a>
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
-        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
-    <script src="{{ asset('js/html5-qrcode.min.js') }}"></script>
 
-    <script>
-        function onScanSuccess(decodedText, decodedResult) {
-            // Handle on success condition with the decoded text or result.
-            console.log(`Scan result: ${decodedText}`, decodedResult);
-            // $('#qrcode-result').html(decodedText);
-        }
-
-        var html5QrcodeScanner = new Html5QrcodeScanner(
-            "reader", {
-                fps: 6,
-                qrbox: 250
-            });
-        html5QrcodeScanner.render(onScanSuccess);
-    </script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\IndexController;
-
+use App\Http\Controllers\QrcodeController;
 
 Route::get('/', [IndexController::class, 'index']);
+Route::get('/qrcode', [QrcodeController::class, 'qrcode']);
 Route::post('/upload', [IndexController::class, 'upload']);
