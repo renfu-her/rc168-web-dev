83cd4118344a452bcd6e97afe287ffdc0c786825
Patrick Her <renfu.her@gmail.com>
2023-10-07 18:43:20 +0800

    update qrcode scanner

--- resources/views/qrcode-scanner.blade.php
+++ resources/views/qrcode-scanner.blade.php
@@ -1,54 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>QRCode</title>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/html5-qrcode.min.js') }}"></script>
-
-</head>
-
+    <title>Html-Qrcode Demo</title>
 <body>
-
-    <video id="video" width="100%" height="auto"></video>
-    <div id="qrCodeResult"></div>
-
-    <script>
-        $(document).ready(function() {
-            // 獲取相機視頻元素和 QR Code 結果元素
-            // 獲取相機視頻元素和 QR Code 結果元素
-            const videoElement = $('#video')[0];
-            const qrCodeResultElement = $('#qrCodeResult');
-
-            // 初始化 html5-qrcode
-            const html5QrCode = new Html5Qrcode();
-
-            // 啟動相機並讀取 QR Code
-            html5QrCode.start({
-                videoId: 'video',
-                qrCodeSuccessCallback: onScanSuccess,
-                qrCodeErrorCallback: onScanError
-            }, {
-                fps: 10,
-                qrbox: 250
-            });
-
-            // 處理讀取到的 QR Code
-            function onScanSuccess(qrCodeMessage) {
-                console.log(qrCodeMessage);
-                // 根據需要處理 QR Code 訊息的邏輯
-            }
-
-            // 處理相機讀取錯誤
-            function onScanError(error) {
-                console.error(error);
-            }
-        });
-    </script>
+    <div id="qr-reader" style="width:500px"></div>
+    <div id="qr-reader-results"></div>
 </body>
+<script src="{{ asset('js/html5-qrcode.min.js') }}"></script>
+<script>
+    function docReady(fn) {
+        // see if DOM is already available
+        if (document.readyState === "complete"
+            || document.readyState === "interactive") {
+            // call on next available tick
+            setTimeout(fn, 1);
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
+    }
 
+    docReady(function () {
+        var resultContainer = document.getElementById('qr-reader-results');
+        var lastResult, countResults = 0;
+        function onScanSuccess(decodedText, decodedResult) {
+            if (decodedText !== lastResult) {
+                ++countResults;
+                lastResult = decodedText;
+                // Handle on success condition with the decoded message.
+                console.log(`Scan result ${decodedText}`, decodedResult);
+            }
+        }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "qr-reader", { fps: 10, qrbox: 250 });
+        html5QrcodeScanner.render(onScanSuccess);
+    });
+</script>
+</head>
 </html>
