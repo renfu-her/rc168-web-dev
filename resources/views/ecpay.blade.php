888e01314b991aa0cbc8293d40561d3115a826fc
Patrick Her <renfu.her@gmail.com>
2023-11-10 10:15:33 +0800

    update ecpay

--- resources/views/ecpay.blade.php
+++ resources/views/ecpay.blade.php
@@ -8,19 +8,8 @@
 </head>
 <body>
     
-    <form action="{{ $result['EcpayURL'] }}" method="post" id="ecpay-post">
-        <input type="hidden" name="MerchantID" value="{{ $result['MerchantID'] }}">
-        <input type="hidden" name="MerchantTradeNo" value="{{ $result['MerchantTradeNo'] }}">
-        <input type="hidden" name="MerchantTradeDate" value="{{ $result['MerchantTradeDate'] }}">
-        <input type="hidden" name="PaymentType" value="{{ $result['PaymentType'] }}">
-        <input type="hidden" name="TotalAmount" value="{{ $result['TotalAmount'] }}">
-        <input type="hidden" name="TradeDesc" value="{{ $result['TradeDesc'] }}">
-        <input type="hidden" name="ItemName" value="{{ $result['ItemName'] }}">
-        <input type="hidden" name="ReturnURL" value="{{ $result['ReturnURL'] }}">
-        <input type="hidden" name="OrderResultURL" value="{{ $result['OrderResultURL'] }}">
-        <input type="hidden" name="ChoosePayment" value="{{ $result['ChoosePayment'] }}">
-        <input type="hidden" name="EncryptType" value="{{ $result['EncryptType'] }}">
-        <input type="hidden" name="CheckMacValue" value="{{ $result['CheckMacValue'] }}">
+    <form action="{{ $result->EcpayURL }}" method="post" id="ecpay-post">
+        <input type="hidden" name="MerchantID" value="{{ $result->MerchantID }}">
     </form>
 
     <script>
