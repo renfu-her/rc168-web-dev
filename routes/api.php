5436eff21696090a8380ab19378e85e7775687bb
Patrick Her <renfu.her@gmail.com>
2023-11-13 22:18:54 +0800

    api.php

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::post('/user_token/check', [UserTokenController::class, 'store'])->name('u
 Route::group(['prefix' => 'user'], function () {
     Route::post('/client/write', [CaseClientController::class, 'store'])->name('user.client.store');
     Route::post('/client/view', [CaseClientController::class, 'view'])->name('user.client.view');
-    Route::get('/client/getAll', [CaseJoinController::class, 'getAll'])->name('user.join.getAll');
+    Route::get('/client/getAll', [CaseClientController::class, 'getAll'])->name('user.join.getAll');
 
     Route::post('/join/view', [CaseJoinController::class, 'view'])->name('user.join.view');
 });
