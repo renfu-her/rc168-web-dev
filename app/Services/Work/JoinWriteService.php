e6ab6d61b9b090743ba0522961452ef51f87a3c2
Patrick Her <renfu.her@gmail.com>
2023-11-21 17:38:32 +0800

    update

--- app/Services/Work/JoinWriteService.php
+++ app/Services/Work/JoinWriteService.php
@@ -98,7 +98,7 @@ class JoinWriteService extends Service
         if (!empty($data['userToken'])) {
             $userToken = UserToken::where('user_token', $data['userToken'])->first();
             if (!empty($userToken)) {
-                $userJoin = CaseJoin::where('case_client_id', $data['itemId'])->where('status', '<', 1)->get();
+                $userJoin = CaseJoin::where('case_client_id', $data['itemId'])->get();
                 foreach ($userJoin as $joinKey => $joinValue) {
                     $caseClient = CaseClient::where('id', $joinValue->id)->first();
                     if (!empty($caseClient)) {
