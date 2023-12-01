3d376c3d27b397041313e560b158ac84af802ae4
Patrick Her <renfu.her@gmail.com>
2023-12-01 10:25:58 +0800

    update

--- resources/views/ecpay.blade.php
+++ resources/views/ecpay.blade.php
@@ -9,25 +9,25 @@
 <body>
     
     <form action="{{ $resArray['EcpayURL'] }}" method="post" id="ecpay-post">
-        <input type="hidden" name="MerchantID" value="{{ $resArray['MerchantID'] }}">
-        <input type="hidden" name="MerchantTradeNo" value="{{ $resArray['MerchantTradeNo'] }}">
-        <input type="hidden" name="MerchantTradeDate" value="{{ $resArray['MerchantTradeDate'] }}">
-        <input type="hidden" name="PaymentType" value="{{ $resArray['PaymentType'] }}">
-        <input type="hidden" name="TotalAmount" value="{{ $resArray['TotalAmount'] }}">
-        <input type="hidden" name="TradeDesc" value="{{ $resArray['TradeDesc'] }}">
-        <input type="hidden" name="ItemName" value="{{ $resArray['ItemName'] }}">
-        <input type="hidden" name="ReturnURL" value="{{ $resArray['ReturnURL'] }}">
-        <input type="hidden" name="OrderResultURL" value="{{ $resArray['OrderResultURL'] }}">
-        <input type="hidden" name="ChoosePayment" value="{{ $resArray['ChoosePayment'] }}">
-        <input type="hidden" name="EncryptType" value="{{ $resArray['EncryptType'] }}">
-        <input type="hidden" name="CheckMacValue" value="{{ $resArray['CheckMacValue'] }}">
-        <input type="hidden" name="NeedExtraPaidInfo" value="{{ $resArray['NeedExtraPaidInfo'] }}">
-
+        <input name="MerchantID" value="{{ $resArray['MerchantID'] }}">
+        <input name="MerchantTradeNo" value="{{ $resArray['MerchantTradeNo'] }}">
+        <input name="MerchantTradeDate" value="{{ $resArray['MerchantTradeDate'] }}">
+        <input name="PaymentType" value="{{ $resArray['PaymentType'] }}">
+        <input name="TotalAmount" value="{{ $resArray['TotalAmount'] }}">
+        <input name="TradeDesc" value="{{ $resArray['TradeDesc'] }}">
+        <input name="ItemName" value="{{ $resArray['ItemName'] }}">
+        <input name="ReturnURL" value="{{ $resArray['ReturnURL'] }}">
+        <input name="OrderResultURL" value="{{ $resArray['OrderResultURL'] }}">
+        <input name="ChoosePayment" value="{{ $resArray['ChoosePayment'] }}">
+        <input name="EncryptType" value="{{ $resArray['EncryptType'] }}">
+        <input name="CheckMacValue" value="{{ $resArray['CheckMacValue'] }}">
+        <input name="NeedExtraPaidInfo" value="{{ $resArray['NeedExtraPaidInfo'] }}">
+        <button type="submit">送出</button> 
     </form>
 
     <script>
         window.addEventListener('load', function() {
-            document.getElementById('ecpay-post').submit();
+            // document.getElementById('ecpay-post').submit();
         });
     </script>
 </body>
