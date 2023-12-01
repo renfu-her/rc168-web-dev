aba9f7d042999b3cf9b4bc01ba5346c824fc437f
Patrick Her <renfu.her@gmail.com>
2023-12-01 10:09:12 +0800

    update

--- app/Http/Controllers/EcpayController.php
+++ app/Http/Controllers/EcpayController.php
@@ -12,9 +12,9 @@ class EcpayController extends Controller
     public function index(Request $request)
     {
         $res = Http::withoutVerifying()->post('https://api-dev.besttour.com.tw/api/payment/ecpay', [
-            "item_description" => "item description",
-            "item_name" => "item name",
-            "order_no" => "OID" . date('YmdHis'),
+            "item_description" => "KKKK-團費",
+            "item_name" => "KKKK-團費",
+            "order_no" => "12345" . date('YmdHis'),
             "total_amount" => 100,
         ]);
 
