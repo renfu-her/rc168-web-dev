ae9cf33eaeaaa85c8c0c49aff9cb67f1edd5c9f9
Patrick Her <renfu.her@gmail.com>
2023-12-12 22:07:58 +0800

    update send mail

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -14,10 +14,13 @@ class SendMailController extends Controller
     {
         $data = $request->all();
 
-        dd($data);
-
-        Mail::send('emails.test', $data, function ($message) use ($data) {
-            $message->to('renfu.her@gmail.com')->subject('測試 ' . date('YmdHis'));
+        Mail::send([], $data, function ($message) use ($data) {
+            $message
+                ->from($data['from']['email'], $data['from']['name'])
+                ->to($data['to']['email'], $data['to']['name'])
+                ->replyTo($data['reply_to']['email'], $data['reply_to']['name'])
+                ->subject($data['subject'])
+                ->setBody($data['body'], "text/html; charset=utf-8");
         });
 
         return '';
