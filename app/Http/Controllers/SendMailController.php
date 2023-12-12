43659301bc3d2652289636d2d00ffb0c53c52d6c
Patrick Her <renfu.her@gmail.com>
2023-12-12 22:24:57 +0800

    update

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -15,15 +15,15 @@ class SendMailController extends Controller
     {
         $data = $request->all();
 
-        $data['body'] = new HtmlString($data['body']);
+        $body = new HtmlString($data['body']);
 
-        Mail::send([], [], function ($message) use ($data) {
+        Mail::send([], [], function ($message) use ($data, $body) {
             $message
                 ->from($data['from']['email'], $data['from']['name'])
                 ->to($data['to']['email'], $data['to']['name'])
                 ->replyTo($data['reply_to']['email'], $data['reply_to']['name'])
                 ->subject($data['subject'])
-                ->setBody($data['body'], "text/html");
+                ->setBody($body, "text/html");
         });
 
         return '';
