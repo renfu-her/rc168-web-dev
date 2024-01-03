39b7b38e738f19bc93fd94ea4780f6abcdcd721c
Patrick Her <renfu.her@gmail.com>
2024-01-03 20:51:31 +0800

    update

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class ProductController extends Controller
+{
+    public function content(Request $request, $product_id)
+    {
+
+        $res = Http::withoutVerifying()->get(config('app.url') . '/api/product/detail/content/' . $product_id);
+
+        $result = $res->body();
+        
+        return view('productContent', compact('result'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ImageUploadController;
 use App\Http\Controllers\QRCodeController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\EcpayController;
+// use App\Http\Controllers\EcpayController;
+
+
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::post('/upload/image', [ImageUploadController::class, 'imageUpload'])->name('image.upload');
 // Route::get('/qrcode-scanner', [QRCodeController::class, 'QRCodeScanner'])->name('qrcode.scanner');
 
-Route::get('/ecpay', [EcpayController::class, 'index']);
+// Route::get('/ecpay', [EcpayController::class, 'index']);
+
+Route::get('/product/content/{id}', [ProductController::class, 'content']);
 
