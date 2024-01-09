a50bc635a6312e853488d6d7714d7fb31a84536f
Patrick Her <renfu.her@gmail.com>
2024-01-09 21:50:44 +0800

    update test

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,20 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ImageUploadController;
 use App\Http\Controllers\QRCodeController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TestController;
+
 // use App\Http\Controllers\EcpayController;
 
 
 
-Route::get('/', [HomeController::class, 'index']);
+route::get('/', [HomeController::class, 'index']);
 
-Route::post('/upload/image', [ImageUploadController::class, 'imageUpload'])->name('image.upload');
+route::post('/upload/image', [ImageUploadController::class, 'imageUpload'])->name('image.upload');
 // Route::get('/qrcode-scanner', [QRCodeController::class, 'QRCodeScanner'])->name('qrcode.scanner');
 
 // Route::get('/ecpay', [EcpayController::class, 'index']);
 
-Route::get('/product/content/{id}', [ProductController::class, 'content']);
+route::get('/product/content/{id}', [ProductController::class, 'content']);
+
+route::post('/test/url', [TestController::class, 'url']);
 
