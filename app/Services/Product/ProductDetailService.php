85462debfb3895a04149c9f217465844045f3c4f
Patrick Her <renfu.her@gmail.com>
2024-01-30 15:43:05 +0800

    update

--- app/Services/Product/ProductDetailService.php
+++ app/Services/Product/ProductDetailService.php
@@ -92,7 +92,7 @@ class ProductDetailService extends Service
 
         $customer = Http::get($this->api_url . '/gws_customer&customer_id=' . $customerId . '&api_key=' . $this->api_key);
         $customerData = $customer->json()['customer'];
-        dd($customerId);
+        dd($customerData);
         $countryId = $customerData[0]['country_id'];
         $zoneId = $customerData[0]['zone_id'];
 
