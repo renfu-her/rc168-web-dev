8d7c14546a2928df9fafad5a2f63fd14ddd0f030
Patrick Her <renfu.her@gmail.com>
2024-01-31 09:09:06 +0800

    update

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -7,7 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PDF</title>
 
-    <script src="{{ asset('js/pdf.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.min.js"
+        integrity="sha512-Z8CqofpIcnJN80feS2uccz+pXWgZzeKxDsDNMD/dJ6997/LSRY+W4NmEt9acwR+Gt9OHN0kkI1CTianCwoqcjQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 </head>
 
