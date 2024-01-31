ad00e01430e226dc6946c081a22f1a2a1c49de03
Patrick Her <renfu.her@gmail.com>
2024-01-31 09:16:29 +0800

    update

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -7,44 +7,67 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PDF</title>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.min.js"
-        integrity="sha512-Z8CqofpIcnJN80feS2uccz+pXWgZzeKxDsDNMD/dJ6997/LSRY+W4NmEt9acwR+Gt9OHN0kkI1CTianCwoqcjQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
+    <script src="{{ asset('js/pdf.mjs') }}" type="module"></script>
 </head>
 
 <body>
-    <script>
-        var url = "{{ asset('pdf/Eg01.pdf') }}";
-        PDFJS.workerSrc = "//mozilla.github.io/pdf.js/build/pdf.worker.js";
-        var loadingTask = PDFJS.getDocument(url);
-        loadingTask.promise.then(
-            function(pdf) {
-                console.log("PDF loaded");
-                var pageNumber = 1;
-                pdf.getPage(pageNumber).then(function(page) {
-                    console.log("Page loaded");
-                    var scale = 1.5;
-                    var viewport = page.getViewport(scale);
-                    var canvas = document.getElementById("example");
-                    var context = canvas.getContext("2d");
-                    canvas.height = viewport.height;
-                    canvas.width = viewport.width;
-                    var renderContext = {
-                        canvasContext: context,
-                        viewport: viewport
-                    };
-                    var renderTask = page.render(renderContext);
-                    renderTask.then(function() {
-                        console.log("Page rendered");
-                    });
+
+    <script type="module">
+        // If absolute URL from the remote server is provided, configure the CORS
+        // header on that server.
+        var url = '{{ asset('pdf/Eg01.pdf') }}';
+
+        // Loaded via <script> tag, create shortcut to access PDF.js exports.
+        var {
+            pdfjsLib
+        } = globalThis;
+
+        // The workerSrc property shall be specified.
+        pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.mjs';
+
+        // Asynchronous download of PDF
+        var loadingTask = pdfjsLib.getDocument(url);
+        loadingTask.promise.then(function(pdf) {
+            console.log('PDF loaded');
+
+            // Fetch the first page
+            var pageNumber = 1;
+            pdf.getPage(pageNumber).then(function(page) {
+                console.log('Page loaded');
+
+                var scale = 1.5;
+                var viewport = page.getViewport({
+                    scale: scale
                 });
-            },
-            function(reason) {
-                console.error(reason);
-            }
-        );
+
+                // Prepare canvas using PDF page dimensions
+                var canvas = document.getElementById('the-canvas');
+                var context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                // Render PDF page into canvas context
+                var renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                };
+                var renderTask = page.render(renderContext);
+                renderTask.promise.then(function() {
+                    console.log('Page rendered');
+                });
+            });
+        }, function(reason) {
+            // PDF loading error
+            console.error(reason);
+        });
     </script>
+
+    <h1>PDF.js 'Hello, world!' example</h1>
+
+    <p>Please use <a href="https://mozilla.github.io/pdf.js/getting_started/#download"><i>official releases</i></a> in
+        production environments.</p>
+
+    <canvas id="the-canvas"></canvas>
 </body>
 
 </html>
