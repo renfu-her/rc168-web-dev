19163e622695e7d60d57d52b2dbd7597e3588d1f
Patrick Her <renfu.her@gmail.com>
2024-02-02 17:07:12 +0800

    update

--- app/Http/Controllers/Api/ProductPaymentController.php
+++ app/Http/Controllers/Api/ProductPaymentController.php
@@ -25,17 +25,15 @@ class ProductPaymentController extends Controller
 
         $data = json_decode($content, true);
 
-        if($data['payment_method'] == 'ecpaypayment'){
-            return $this->ecpay($data);
-        } elseif($data['payment_method'] == 'linepay_sainent'){
-            return $this->linepay($data);
+        if ($data['payment_method'] == 'ecpaypayment') {
+            return $this->ecpay($data, $req);
+        } elseif ($data['payment_method'] == 'linepay_sainent') {
+            return $this->linepay($data, $req);
         }
-
-        
     }
 
     // ecpay
-    public function ecpay($data)
+    public function ecpay($data, $req)
     {
 
         // $addressId = $data['address_id'];
@@ -76,11 +74,76 @@ class ProductPaymentController extends Controller
     }
 
     // line pay
-    public function linepay($data)
+    public function linepay($data, $req)
     {
 
+        $total = 0;
+        $itemDescription = '';
+        foreach ($data['products'] as $key => $value) {
+            $items[$key] = [
+                'name' => $value['name'],
+                'quantity' => $value['quantity'],
+                'price' => $value['price'],
+            ];
+            $itemDescription .= $value['name'] . '|';
+            $total += $value['total'];
+        }
 
-        dd($data);
+        if (!empty($data['shipping_cost'])) {
+            $items[count($data['products']) + 1] = [
+                'name' => '運費',
+                'quantity' => 1,
+                'price' => $data['shipping_cost'],
+            ];
+        }
+
+        $total += $data['shipping_cost'];
+
+        $order = [
+            "amount" => $total,
+            "order_id" => $req['order_id'],
+            "packages" => [
+                [
+                    "id" => "0001",
+                    "amount" => $total,
+                    "name" => $itemDescription,
+                    "products" => [
+                        $items
+                    ]
+
+                ]
+            ]
+        ];
+
+        $orderRequest = new Request(['order' => $order]);
+        dd($orderRequest);
+    }
+
+    public function confirm(Request $request)
+    {
+
+        $req = $request->all();
+
+        $linePay = new \yidas\linePay\Client([
+            'channelId' => config('line_pay.LINE_PAY_CHANNEL_ID'),
+            'channelSecret' => config('line_pay.LINE_PAY_CHANNEL_SECRET'),
+            'isSandbox' => config('line_pay.LINE_PAY_SANDBOX')
+        ]);
+
+        // confirm
+        $confirm = $linePay->confirm($req['transactionId'], [
+            "amount" => $order->amount,
+            "currency" => 'TWD',
+        ]);
+
+        // detail,套件寫錯，陣列已經廢除
+        $detail = $linePay->details([
+            "transactionId" => $req['transactionId'],
+        ]);
+
+        $detailArray = $detail->toArray();
+
+        $apiRes = $detailArray['info'][0];
     }
 
     // 付款結果
@@ -89,7 +152,7 @@ class ProductPaymentController extends Controller
 
         $data = $request->all();
 
-        if($data['RtnCode'] == 1){
+        if ($data['RtnCode'] == 1) {
             $msg = '付款已經完成';
         } else {
             $msg = '付款失敗';
