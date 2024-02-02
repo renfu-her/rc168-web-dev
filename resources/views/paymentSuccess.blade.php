e71aa033c19595924a0f7702330741274bb1df4c
Patrick Her <renfu.her@gmail.com>
2024-02-02 14:34:43 +0800

    update

--- resources/views/paymentSuccess.blade.php
+++ resources/views/paymentSuccess.blade.php
@@ -14,7 +14,7 @@
 
     <div class="container">
         <div class="d-flex justify-content-center align-items-center" style="width: 100%; height: 100%">
-            <div class="col-12 text-center" style="margin-top: 45%">
+            <div class="col-12 text-center" style="margin-top: 250px">
                 <h4 style="color: blue">付款已經完成</h4>
             </div>
         </div>
