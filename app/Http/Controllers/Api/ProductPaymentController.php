f121bc86a769366e9b9f10958f9936422d586720
Patrick Her <renfu.her@gmail.com>
2024-02-02 08:30:57 +0800

    update payment

--- app/Http/Controllers/Api/ProductPaymentController.php
+++ app/Http/Controllers/Api/ProductPaymentController.php
@@ -24,8 +24,6 @@ class ProductPaymentController extends Controller
         $content = Storage::disk('public')->get( $req['customerId'] . '.txt');
 
         $data = json_decode($content, true);
-
-        dd($data);
         
         // $addressId = $data['address_id'];
         $customerId = $data['customer'][0]['customer_id'];
