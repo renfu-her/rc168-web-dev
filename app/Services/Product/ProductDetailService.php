38d06f428a3102f92d7e02cba0a35f88d929b2ed
Patrick Her <renfu.her@gmail.com>
2024-06-05 10:19:58 +0800

    Refactor setOrder method in ProductDetailService for improved order submission process

--- app/Services/Product/ProductDetailService.php
+++ app/Services/Product/ProductDetailService.php
@@ -88,34 +88,67 @@ class ProductDetailService extends Service
 
     public function setOrder($customerId)
     {
-
         $orderData = OrderData::where('customer_id', $customerId)->first();
+        if (!$orderData) {
+            return $this->response = Service::response('error', 'Order data not found.');
+        }
 
         $data = json_decode($orderData->data, true);
 
-        $orderLog = new OrderLog();
-        $orderLog->events = '訂單建立';
-        $orderLog->logs = json_encode($data);
-        $orderLog->save();
-
-        Storage::disk('public')->put('data-' . $customerId, json_encode($data));
+        $this->logOrderEvent('訂單建立', $data);
 
         $addressId = $data['address_id'];
         $customerId = $data['customer'][0]['customer_id'];
 
-        $address = Http::get($this->api_url . '/gws_customer_address&customer_id=' . $customerId . '&address_id=' . $addressId . '&api_key=' . $this->api_key);
-        $addressData = $address->json()['customer_address'];
+        $addressData = $this->fetchData('gws_customer_address', [
+            'customer_id' => $customerId,
+            'address_id' => $addressId
+        ])['customer_address'];
 
-        $customer = Http::get($this->api_url . '/gws_customer&customer_id=' . $customerId . '&api_key=' . $this->api_key);
-        $customerData = $customer->json()['customer'];
+        $customerData = $this->fetchData('gws_customer', [
+            'customer_id' => $customerId
+        ])['customer'];
 
         $countryId = $addressData[0]['country_id'];
         $zoneId = $addressData[0]['zone_id'];
 
-        // dd($data['customer'][0]['customer_id']);
+        $submitData = $this->buildSubmitData($data, $customerData, $addressData, $countryId, $zoneId);
+        $this->addCountryAndZoneNames($submitData, $countryId, $zoneId);
 
+        ksort($submitData);
+
+        $this->logOrderEvent('訂單 submitData', $submitData);
+
+        $result = Http::asForm()->post($this->api_url . '/gws_appcustomer_order/add&customer_id=' . $customerId . '&api_key=' . $this->api_key, $submitData);
+
+        if ($result->failed()) {
+            return $this->response = Service::response('error', 'Order submission failed.', $result->json());
+        }
+
+        return $this->response = Service::response('success', 'OK', $result->json());
+    }
+
+    private function logOrderEvent($event, $data)
+    {
+        $orderLog = new OrderLog();
+        $orderLog->events = $event;
+        $orderLog->logs = json_encode($data);
+        $orderLog->save();
+    }
+
+    private function fetchData($endpoint, $params)
+    {
+        $response = Http::get($this->api_url . "/$endpoint", array_merge($params, ['api_key' => $this->api_key]));
+        if ($response->failed()) {
+            throw new Exception("Failed to fetch data from $endpoint");
+        }
+        return $response->json();
+    }
+
+    private function buildSubmitData($data, $customerData, $addressData, $countryId, $zoneId)
+    {
         $submitData = [
-            'customer[customer_id]' => $customerId,
+            'customer[customer_id]' => $customerData[0]['customer_id'],
             'customer[customer_group_id]' => 1,
             'customer[firstname]' => $customerData[0]['firstname'],
             'customer[lastname]' => $customerData[0]['lastname'],
@@ -136,7 +169,6 @@ class ProductDetailService extends Service
             'payment_address[zone_id]' => $zoneId,
             'payment_address[custom_field][1]' => '711',
 
-
             // shipping_address
             'shipping_address[firstname]' => $customerData[0]['firstname'],
             'shipping_address[lastname]' => $customerData[0]['lastname'],
@@ -151,15 +183,80 @@ class ProductDetailService extends Service
             'shipping_address[custom_field][1]' => '711',
         ];
 
+        $this->addProductsToSubmitData($submitData, $data['products']);
+        $this->addTotalsToSubmitData($submitData, $data['totals']);
 
-        // country name
-        $country = Http::get($this->api_url . '/gws_country&country_id=' . $countryId . '&api_key=' . $this->api_key);
-        $countryData = $country->json()['country'];
+        $submitData['total'] = array_reduce($data['products'], function ($carry, $item) {
+            return $carry + $item['total'];
+        }, 0);
+
+        $this->addPaymentMethod($submitData, $data['payment_method']);
+
+        return $submitData;
+    }
+
+    private function addProductsToSubmitData(&$submitData, $products)
+    {
+        foreach ($products as $key => $product) {
+            $submitData["products[$key][product_id]"] = $product['product_id'];
+            $submitData["products[$key][model]"] = $product['name'];
+            $submitData["products[$key][name]"] = $product['name'];
+            $submitData["products[$key][quantity]"] = $product['quantity'];
+            $submitData["products[$key][price]"] = $product['price'];
+            $submitData["products[$key][total]"] = $product['total'];
+            $submitData["products[$key][tax_class_id]"] = 9;
+            $submitData["products[$key][download]"] = '';
+            $submitData["products[$key][subtract]"] = 1;
+            $submitData["products[$key][reward]"] = 0;
+
+            if (isset($product['options']) && is_array($product['options'])) {
+                foreach ($product['options'] as $optionKey => $optionValue) {
+                    $submitData["products[$key][option][$optionKey][product_option_id]"] = $optionValue['product_option_id'];
+                    $submitData["products[$key][option][$optionKey][product_option_value_id]"] = $optionValue['product_option_value_id'];
+                    $submitData["products[$key][option][$optionKey][name]"] = $optionValue['name'];
+                    $submitData["products[$key][option][$optionKey][value]"] = $optionValue['value'];
+                    $submitData["products[$key][option][$optionKey][type]"] = $optionValue['type'];
+                }
+            }
+        }
+    }
+
+    private function addTotalsToSubmitData(&$submitData, $totals)
+    {
+        foreach ($totals as $key => $total) {
+            $submitData["totals[$key][code]"] = $total['code'];
+            $submitData["totals[$key][title]"] = $total['title'];
+            $submitData["totals[$key][value]"] = str_replace('$', '', $total['text']);
+            $submitData["totals[$key][sort_order]"] = $key + 1;
+        }
+    }
+
+    private function addPaymentMethod(&$submitData, $paymentMethod)
+    {
+        $methods = [
+            'linepay_sainent' => ['title' => 'LINE Pay', 'code' => 'linepay_sainent'],
+            'ecpaypayment' => ['title' => '線上刷卡', 'code' => 'ecpaypayment'],
+            'default' => ['title' => '銀行轉帳', 'code' => 'bank_transfer']
+        ];
+
+        $method = $methods[$paymentMethod] ?? $methods['default'];
+        $submitData['payment_method[title]'] = $method['title'];
+        $submitData['payment_method[code]'] = $method['code'];
+    }
+
+    private function addCountryAndZoneNames(&$submitData, $countryId, $zoneId)
+    {
+        $countryData = $this->fetchData('gws_country', [
+            'country_id' => $countryId
+        ])['country'];
+
         $submitData["payment_address[country]"] = $countryData[0]['name'];
         $submitData["shipping_address[country]"] = $countryData[0]['name'];
 
-        $zone = Http::get($this->api_url . '/gws_zone&country_id=' . $countryId . '&api_key=' . $this->api_key);
-        $zoneData = $zone->json()['zones'];
+        $zoneData = $this->fetchData('gws_zone', [
+            'country_id' => $countryId
+        ])['zones'];
+
         foreach ($zoneData as $value) {
             if ($value['zone_id'] == $zoneId) {
                 $submitData["payment_address[zone]"] = $value['name'];
@@ -167,89 +264,15 @@ class ProductDetailService extends Service
             }
         }
 
-        $submitData['payment_address[address_format]'] = $addressData[0]['postcode'] . ' ' . $countryData[0]['name'] . $submitData["payment_address[zone]"] . $addressData[0]['address_1'];
-        $submitData['shipping_address[address_format]'] = $addressData[0]['postcode'] . ' ' . $countryData[0]['name'] . $submitData["payment_address[zone]"] . $addressData[0]['address_1'];
-
-        // payment
-        $payment_method = $data['payment_method'];
-        if ($payment_method == 'linepay_sainent') {
-            $submitData['payment_method[title]'] = "LINE Pay";
-            $submitData['payment_method[code]'] = "linepay_sainent";
-        } elseif ($payment_method == 'ecpaypayment') {
-            $submitData['payment_method[title]'] = "線上刷卡";
-            $submitData['payment_method[code]'] = "ecpaypayment";
-        } else {
-            $submitData['payment_method[title]'] = "銀行轉帳";
-            $submitData['payment_method[code]'] = "bank_transfer";
-        }
-
-        // product array
-        $total = 0;
-        foreach ($data['products'] as $key => $value) {
-            $submitData["products[" . $key . "][product_id]"] = $value['product_id'];
-            $submitData["products[" . $key . "][model]"] = $value['name'];
-            $submitData["products[" . $key . "][name]"] = $value['name'];
-            $submitData["products[" . $key . "][quantity]"] = $value['quantity'];
-            $submitData["products[" . $key . "][price]"] = $value['price'];
-            $submitData["products[" . $key . "][total]"] = $value['total'];
-            $submitData["products[" . $key . "][tax_class_id]"] = 9;
-            $submitData["products[" . $key . "][download]"] = '';
-            $submitData["products[" . $key . "][subtract]"] = 1;
-            $submitData["products[" . $key . "][reward]"] = 0;
-
-            // 處理選項
-            if (isset($value['options']) && is_array($value['options'])) {
-                foreach ($value['options'] as $optionKey => $optionValue) {
-                    $submitData["products[" . $key . "][option][" . $optionKey . "][product_option_id]"] = $optionValue['product_option_id'];
-                    $submitData["products[" . $key . "][option][" . $optionKey . "][product_option_value_id]"] = $optionValue['product_option_value_id'];
-                    $submitData["products[" . $key . "][option][" . $optionKey . "][name]"] = $optionValue['name'];
-                    $submitData["products[" . $key . "][option][" . $optionKey . "][value]"] = $optionValue['value'];
-                    $submitData["products[" . $key . "][option][" . $optionKey . "][type]"] = $optionValue['type'];
-                }
-            }
-
-            $total += $value['total'];
-        }
-
-        foreach ($data['totals'] as $key => $totals) {
-            $submitData["totals[$key][code]"] = $totals['code'];
-            $submitData["totals[$key][title]"] = $totals['title'];
-            $submitData["totals[$key][value]"] = str_replace('$', '', $totals['text']);
-            $submitData["totals[$key][sort_order]"] = $key + 1;
-        }
-
-        $submitData['total'] = $total;
-        // $submitData["totals[0][code]"] = "sub_total";
-        // $submitData["totals[0][title]"] = "Sub-Total";
-        // $submitData["totals[0][value]"] = $total;
-        // $submitData["totals[0][sort_order]"] = "1";
-
-        // shipping_method
-        $submitData["shipping_method[title]"] = "運費";
-        $submitData["shipping_method[code]"] = "flat.flat";
-        $submitData["payment_address[cellphone]"] = "0922013171";
-        $submitData["payment_address[pickupstore]"] = "0922013171";
-        $submitData["shipping_address[cellphone]"] = "0922013171";
-        $submitData["shipping_address[pickupstore]"] = "0922013171";
-
-
-        $orderLog = new OrderLog();
-        $orderLog->events = '訂單 submitData';
-        $orderLog->logs = json_encode($submitData);
-        $orderLog->save();
-        // Storage::disk('public')->put($customerId . ".txt", json_encode($data));
-        // Storage::disk('public')->put('customerId', json_encode($data));
-        // Storage::disk('public')->put('submitData', json_encode($submitData));
-
-        $result = Http::asForm()
-            ->post($this->api_url . '/gws_appcustomer_order/add&customer_id=' . $customerId . '&api_key=' . $this->api_key, $submitData);
-
-        // dd($result->body(), $result->json());
-
-        $this->response = Service::response('success', 'OK', $result->json());
-
-        return $this;
+        $submitData['payment_address[address_format]'] = $this->formatAddress($submitData['payment_address'], $countryData[0]['name']);
+        $submitData['shipping_address[address_format]'] = $this->formatAddress($submitData['shipping_address'], $countryData[0]['name']);
     }
+
+    private function formatAddress($address, $countryName)
+    {
+        return $address['postcode'] . ' ' . $countryName . $address['zone'] . $address['address_1'];
+    }
+
 
 
     public function runValidate($method)
