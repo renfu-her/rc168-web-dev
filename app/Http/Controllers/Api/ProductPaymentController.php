a1534c162ff10d86c6d511071d939508c46d27ae
Patrick Her <renfu.her@gmail.com>
2024-08-23 16:31:00 +0800

    Refactor ProductPaymentController to remove debug statements and improve ecpay method

--- app/Http/Controllers/Api/ProductPaymentController.php
+++ app/Http/Controllers/Api/ProductPaymentController.php
@@ -34,8 +34,6 @@ class ProductPaymentController extends Controller
 
         $data = json_decode($content, true);
 
-        dd($data);
-
         switch ($data['payment_method']) {
             case 'ecpaypayment':
                 $payment = $this->ecpay($data, $req);
@@ -142,6 +140,8 @@ class ProductPaymentController extends Controller
 
         // $addressId = $data['address_id'];
         $customerId = $data['customer'][0]['customer_id'];
+
+        dd($data);
 
         $items = [];
         $itemDescription = '';
